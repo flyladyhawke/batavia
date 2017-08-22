67f3f33642502ce86c6adb026f69f0baecc1fd44
Rose <ladyhawke@gmail.com>
2017-08-23 01:37:28 +1000

    add tests for chr

--- tests/builtins/test_chr.py
+++ tests/builtins/test_chr.py
@@ -1,8 +1,20 @@
+from unittest import expectedFailure
+
 from .. utils import TranspileTestCase, BuiltinFunctionTestCase
 
 
 class ChrTests(TranspileTestCase):
-    pass
+    def test_int(self):
+        self.assertCodeExecution("print(chr(40))")
+
+    @expectedFailure
+    def test_int_too_small(self):
+        self.assertCodeExecution("print(chr(-1))")
+
+    @expectedFailure
+    def test_int_too_large(self):
+        self.assertCodeExecution("print(chr(260))")
+
 
 class BuiltinChrFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
     function = "chr"
@@ -15,7 +27,6 @@ class BuiltinChrFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
         'test_dict',
         'test_float',
         'test_frozenset',
-        'test_int',
         'test_list',
         'test_NotImplemented',
         'test_range',
